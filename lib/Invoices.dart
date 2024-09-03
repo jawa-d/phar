4bdd2f9f7a153969ba82ea60da34c29a998203b0
jawad <kadhimj095@gmail.com>
2024-09-03 22:21:39 +0300

    appbar page invo

--- lib/Invoices.dart
+++ lib/Invoices.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Invoices extends StatefulWidget {
   const Invoices({super.key});
@@ -10,8 +11,15 @@ class Invoices extends StatefulWidget {
 class _InvoicesState extends State<Invoices> {
   @override
   Widget build(BuildContext context) {
-    return  ListView(
+    return  Scaffold(
+      appBar: AppBar(
+          title: Text("Pharmacy", style: GoogleFonts.lato()),
+        actions: const [Icon(Icons.shopping_cart_outlined)],
+        centerTitle: true,
+      ),
       
+  drawer: const Drawer(),
+     
     );
   }
 }
